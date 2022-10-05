8c6c9607c026897dbb3b08daa6a269778bbdbc72
Olga <parlelya@vk.com>
2022-10-05 17:01:17 +0300

    Fix bug with deleting

--- todoList.js
+++ todoList.js
@@ -1,11 +1,9 @@
-// const fromStorage = localStorage.getItem('todo')
-// if (fromStorage) document.querySelector('.todo__items').innerHTML = fromStorage
-
+const fromStorage = localStorage.getItem('todo')
+if (fromStorage) document.querySelector('.todo__items').innerHTML = fromStorage
 const addTask = document.querySelector(".todo__input")
 const removeTask = document.querySelector(".todo__items")
 
 function add() {
-	console.log("выполянется add()")
 	const inputText = document.querySelector('.todo__text')
 	if (!inputText.value.length) return
 	document.querySelector('.todo__items').insertAdjacentHTML('beforeend', create(inputText.value))
@@ -13,11 +11,7 @@ function add() {
 }
 
 function create(text) {
-	console.log("выполянется create(text)")
-	return `<span class="todo__item"><label class="todo__task">
-	<input type="checkbox" name="checkbox" class="todo__item">
-	${text}
-	</label></span>`
+	return `<span class="todo__item"><label class="todo__task"><input type="checkbox" name="checkbox" class="todo__item-check">${text}</label></span>`
 }
 
 function save() {
@@ -25,19 +19,14 @@ function save() {
 }
 
 addTask.addEventListener('click', function(event) {
-	console.log("прослушка на добавление повешена")
-	if (event.target.classList.contains('todo__add')) {
+	if (event.target.closest('.todo__add')) {
 		add()
 		save()
-		console.log("задача добавлена")
+
 	}})
 
 removeTask.addEventListener('click', function(event) {
-	console.log("прослушка на удаление повешена")
-	if (event.target.classList.contains('todo__item-check')) {
+	if (event.target.closest('.todo__item-check')) {
 		event.target.closest('.todo__item').remove()
 		save()
-		console.log("задача выполнена и удалена")
 	}})
-
-//TODO фикс добавления по пробелу, а не по клику на кнопку, фикс удаления добавленных задач
